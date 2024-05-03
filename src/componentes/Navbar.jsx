5164c345068bac30d6872e44cd5c8fbc48e0eeae
Carlos Zalazar <danielzalazar182003@gmail.com>
2024-05-02 22:41:10 -0300

    algunas modificaciones

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,17 +1,27 @@
 // NavBar.js
 
 import React from 'react';
-import CartWidget from './CartWidget';
+import CartWidget from './CartWidget'; // Importa el componente CartWidget
+import './Navbar.css'; // Importa el archivo de estilos CSS
 
 const NavBar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-brand">
-                My Shop
+                <span className="logo">TechMart</span>
             </div>
-            <CartWidget />
+            <ul className="nav-links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            <div className="cart-container">
+                <CartWidget /> {}
+            </div>
         </nav>
     );
 };
 
 export default NavBar;
+
+
